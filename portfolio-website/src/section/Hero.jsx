ad7b7652a64da5c6edcd073c512a10b016bcef85
digvijay164 <digvijaydesai164@gmail.com>
2026-01-29 11:21:17 +0530

    added 3D Astronaut Model in Hero Section

--- portfolio-website/src/section/Hero.jsx
+++ portfolio-website/src/section/Hero.jsx
@@ -1,14 +1,64 @@
 import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import ParallexBackground from "../components/ParallexBackground";
+import { Astronaut } from "../components/Astronaut";
+import { useMediaQuery } from "react-responsive";
+import { Float } from "@react-three/drei";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import { easing } from "maath";
 
-const Hero = ()=>{
-    return(
+const Hero = () => {
+    const isMobile = useMediaQuery({
+        maxWidth: 853
+    })
+    return (
         <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
-            <HeroText/>
-            <ParallexBackground/>
+            <HeroText />
+            <ParallexBackground />
+
+            <figure
+                className="absolute inset-0"
+                style={{
+                    widows: "100vw",
+                    height: "100vh"
+                }}
+            >
+                <Canvas
+                    camera={{
+                        position: [0, 1, 3]
+                    }}
+                >
+                    <Suspense fallback={<Loader />}>
+                        <Float>
+
+                        </Float>
+                    </Suspense>
+                    <Astronaut
+                        scale={isMobile && 0.23}
+                        position={isMobile && [0, -1.5, 0]}
+                    />
+                </Canvas>
+            </figure>
+
         </section>
     )
+}
+
+function Rig(){
+    return useFrame((state, delta)=>{
+        easing.damp3(
+            state.camera.position,
+            [
+                state.mouse.x /10,
+                1 + state.mouse.y /10,
+                1
+            ],
+            0.5,
+            delta
+        )
+    })
 }
 
 export default Hero
